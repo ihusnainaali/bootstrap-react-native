ce7c4eb6fc30b1fa23a9e3965739712e67dc73d7
yuhong <yuhong.wang@xiaoniangao.cm>
2018-05-02 16:29:09 +0800

    Add register logic

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -7,13 +7,17 @@ import config from '../../../aws-exports'
 Amplify.configure(config)
 
 class Register extends Component {
-  state = { // 1
-    authCode: '',
+  state = {
+    username: '',
     email: '',
-    password: ''
+    password: '',
+    passwordConfirmation: ''
   }
   setAuthCode(authCode) { // 2
     this.setState({ authCode });
+  }
+  setUsername(username) {
+    this.setState({ username });
   }
   setEmail(email) {
     this.setState({ email });
@@ -21,25 +25,50 @@ class Register extends Component {
   setPassword(password) {
     this.setState({ password });
   }
-  validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
+  setPasswordConfirmation(passwordConfirmation) {
+    this.setState({ passwordConfirmation });
+  }
+  validateEmail( email ) {
+    var EmailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var eduEndingRegex = /^.*\.edu$|^.*\.edu\.[a-zA-Z]{2,}/
+    var emailString = String(email).toLowerCase()
+    return EmailRegex.test(emailString) && eduEndingRegex.test(emailString);
+  }
+  confirmPassword( p1, p2 ) {
+    return p1 == p2;
+  }
+  validatePassword( password ) {
+    var strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+    return strongPasswordRegex.test(String(password));
+  }
   signUp() {
-    // Auth.signUp({ // 3
-    //   username: 'myCoolUsername',
-    //   password: 'MyCoolP@ssword2!',
-    //   attributes: {
-    //     phone_number: '+15555555555',
-    //     email: 'yourcoolemail@gmail.com'
-    //   }
-    // })
-    //   .then(res => {
-    //     console.log('successful signup: ', res)
-    //   })
-    //   .catch(err => {
-    //     console.log('error signing up: ', err)
-    //   })
+    if (!this.validateEmail(this.state.email)) {
+      console.log('Email has to be a school email address.');
+      return;
+    }
+    if (!this.confirmPassword(this.state.password, this.state.passwordConfirmation)) {
+      console.log('Passwords does not match');
+      return;
+    }
+    if (!this.validatePassword(this.state.password)) {
+      console.log('Password is too weak.');
+      return;
+    }
+
+    Auth.signUp({ // 3
+      username: this.state.username,
+      password: this.state.password,
+      attributes: {
+        phone_number: '+15555555555',
+        email: this.state.email
+      }
+    })
+      .then(res => {
+        console.log('successful signup: ', res)
+      })
+      .catch(err => {
+        console.log('error signing up: ', err)
+      })
   }
   confirmUser() { // 4
     const { authCode } = this.state
@@ -56,18 +85,23 @@ class Register extends Component {
     return (
       <View style={styles.wrapper}>
         <TextInput
+          placeholder='Username'
+          onChangeText={value => this.setUsername(value)}
+          style={styles.input}
+        />
+        <TextInput
           placeholder='Email'
           onChangeText={value => this.setEmail(value)}
           style={styles.input}
         />
         <TextInput
           placeholder='Password'
-          onChangeText={value => this.onChangeText(value)}
+          onChangeText={value => this.setPassword(value)}
           style={styles.input}
         />
         <TextInput
           placeholder='Confirm Password'
-          onChangeText={value => this.onChangeText(value)}
+          onChangeText={value => this.setPasswordConfirmation(value)}
           style={styles.input}
         />
         <Button title='Sign Up' onPress={this.signUp.bind(this)} />
