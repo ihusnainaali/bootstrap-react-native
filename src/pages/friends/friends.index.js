e34baeb78f00c280e80c631ee04ea04949b039fa
Jeffrey Corpuz <jeffcorpuz@icloud.com>
2018-05-10 23:20:07 +0800

    Remove name

--- src/pages/friends/friends.index.js
+++ src/pages/friends/friends.index.js
@@ -1,38 +1,42 @@
 import React , { Component } from 'react';
-import { StyleSheet, Image, View,ListView, Text, TextInput, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
+import { Image, View, ListView, Text, TextInput, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
 
 import { ListItem } from 'react-native-elements';
 import { withNavigation } from 'react-navigation';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      marginTop: 20,
-    },
-  });
   
+const list = [
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'United States'
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'China'
+  },
+]
+
+keyExtractor = (item, index) => index
+
+renderItem = ({ item }) => (
+  <ListItem
+    title={item.name}
+    subtitle={item.subtitle}
+    leftAvatar={{ source: { uri: item.avatar_url } }}
+  />
+)
+
 class Friends extends React.Component {
-
-    constructor(props) {
-      super(props);
-  
-      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-      this.state = {
-        dataSource: ds.cloneWithRows(['row 1', 'row 2']),
-      };
-    }
-    
-    render() {
-      return (
-        <ListView
-          style={styles.container}
-          dataSource={this.state.dataSource}
-          renderRow={(data) => <View><Text>{data}</Text></View>}
-        />
-      );
-    }
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Friends!</Text>
+      </View>
+    );
   }
+}
   
 
 export default withNavigation(Friends);
