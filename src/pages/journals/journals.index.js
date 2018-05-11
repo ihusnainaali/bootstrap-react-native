004685cfb10a74fd0005ff7d2fe6a92c2d776e12
Jeffrey Corpuz <jeffcorpuz@icloud.com>
2018-05-11 21:34:19 +0800

    Comment out journal list

--- src/pages/journals/journals.index.js
+++ src/pages/journals/journals.index.js
@@ -9,127 +9,135 @@ import theme from '../../styles/theme.style';
 
 class Journals extends React.Component {
 
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      loading: false,
-      data: [],
-      page: 1,
-      seed: 1,
-      error: null,
-      refreshing: false
-    };
-  }
+  //   this.state = {
+  //     loading: false,
+  //     data: [],
+  //     page: 1,
+  //     seed: 1,
+  //     error: null,
+  //     refreshing: false
+  //   };
+  // }
 
-  componentDidMount() {
-    this.makeRemoteRequest();
-  }
+  // componentDidMount() {
+  //   this.makeRemoteRequest();
+  // }
   
-  // TODO ADD NETWORKING ROUTING FOR FRIENDS
-  makeRemoteRequest = () => {
-    const { page, seed } = this.state;
-    const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
-    this.setState({ loading: true });
+  // // TODO ADD NETWORKING ROUTING FOR FRIENDS
+  // makeRemoteRequest = () => {
+  //   const { page, seed } = this.state;
+  //   const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
+  //   this.setState({ loading: true });
 
-    fetch(url)
-      .then(res => res.json())
-      .then(res => {
-        this.setState({
-          data: page === 1 ? res.results : [...this.state.data, ...res.results],
-          error: res.error || null,
-          loading: false,
-          refreshing: false
-        });
-      })
-      .catch(error => {
-        this.setState({ error, loading: false });
-      });
-  };
+  //   fetch(url)
+  //     .then(res => res.json())
+  //     .then(res => {
+  //       this.setState({
+  //         data: page === 1 ? res.results : [...this.state.data, ...res.results],
+  //         error: res.error || null,
+  //         loading: false,
+  //         refreshing: false
+  //       });
+  //     })
+  //     .catch(error => {
+  //       this.setState({ error, loading: false });
+  //     });
+  // };
 
-  handleRefresh = () => {
-    this.setState(
-      {
-        page: 1,
-        seed: this.state.seed + 1,
-        refreshing: true
-      },
-      () => {
-        this.makeRemoteRequest();
-      }
-    );
-  };
+  // handleRefresh = () => {
+  //   this.setState(
+  //     {
+  //       page: 1,
+  //       seed: this.state.seed + 1,
+  //       refreshing: true
+  //     },
+  //     () => {
+  //       this.makeRemoteRequest();
+  //     }
+  //   );
+  // };
 
-  handleLoadMore = () => {
-    this.setState(
-      {
-        page: this.state.page + 1
-      },
-      () => {
-        this.makeRemoteRequest();
-      }
-    );
-  };
+  // handleLoadMore = () => {
+  //   this.setState(
+  //     {
+  //       page: this.state.page + 1
+  //     },
+  //     () => {
+  //       this.makeRemoteRequest();
+  //     }
+  //   );
+  // };
 
-  renderSeparator = () => {
-    return (
-      <View
-        style={{
-          height: 1,
-          width: "86%",
-          backgroundColor: theme.COLOR_PRIMARY_DARK,
-          marginLeft: "14%"
-        }}
-      />
-    );
-  };
+  // renderSeparator = () => {
+  //   return (
+  //     <View
+  //       style={{
+  //         height: 1,
+  //         width: "86%",
+  //         backgroundColor: theme.COLOR_PRIMARY_DARK,
+  //         marginLeft: "14%"
+  //       }}
+  //     />
+  //   );
+  // };
 
-  renderHeader = () => {
-    return <SearchBar placeholder="Type Here..." lightTheme round />;
-  };
+  // renderHeader = () => {
+  //   return <SearchBar placeholder="Type Here..." lightTheme round />;
+  // };
 
-  renderFooter = () => {
-    if (!this.state.loading) return null;
+  // renderFooter = () => {
+  //   if (!this.state.loading) return null;
 
-    return (
-      <View
-        style={{
-          paddingVertical: 20,
-          borderTopWidth: 1,
-          borderColor: "#CED0CE"
-        }}
-      >
-        <ActivityIndicator animating size="large" />
-      </View>
-    );
-  };
+  //   return (
+  //     <View
+  //       style={{
+  //         paddingVertical: 20,
+  //         borderTopWidth: 1,
+  //         borderColor: "#CED0CE"
+  //       }}
+  //     >
+  //       <ActivityIndicator animating size="large" />
+  //     </View>
+  //   );
+  // };
+
+  // render() {
+  //   return (
+  //     <KeyboardAvoidingView>
+  //     <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
+  //       <FlatList
+  //         data={this.state.data}
+  //         renderItem={({ item }) => (
+  //           <ListItem
+  //             roundAvatar
+  //             title={`${item.name.first} ${item.name.last}`}
+  //             subtitle={item.email}
+  //             avatar={{ uri: item.picture.thumbnail }}
+  //             containerStyle={{ borderBottomWidth: 0 }}
+  //           />
+  //         )}
+  //         keyExtractor={item => item.email}
+  //         ItemSeparatorComponent={this.renderSeparator}
+  //         ListHeaderComponent={this.renderHeader}
+  //         ListFooterComponent={this.renderFooter}
+  //         onRefresh={this.handleRefresh}
+  //         refreshing={this.state.refreshing}
+  //         onEndReached={this.handleLoadMore}
+  //         onEndReachedThreshold={20}
+  //       />
+  //     </List>
+  //     </KeyboardAvoidingView>
+  //   );
+  // }
 
   render() {
     return (
-      <KeyboardAvoidingView>
-      <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
-        <FlatList
-          data={this.state.data}
-          renderItem={({ item }) => (
-            <ListItem
-              roundAvatar
-              title={`${item.name.first} ${item.name.last}`}
-              subtitle={item.email}
-              avatar={{ uri: item.picture.thumbnail }}
-              containerStyle={{ borderBottomWidth: 0 }}
-            />
-          )}
-          keyExtractor={item => item.email}
-          ItemSeparatorComponent={this.renderSeparator}
-          ListHeaderComponent={this.renderHeader}
-          ListFooterComponent={this.renderFooter}
-          onRefresh={this.handleRefresh}
-          refreshing={this.state.refreshing}
-          onEndReached={this.handleLoadMore}
-          onEndReachedThreshold={50}
-        />
-      </List>
-      </KeyboardAvoidingView>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Journals!</Text>
+      </View>
     );
   }
 }
