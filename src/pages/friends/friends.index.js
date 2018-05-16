62718f333ef5baf4bc603843f0b334d754bb06fb
Jeffrey Corpuz <jeffcorpuz@icloud.com>
2018-05-16 16:01:45 +0800

    Begin work to refactor friend/journal list for native base

--- src/pages/friends/friends.index.js
+++ src/pages/friends/friends.index.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import { KeyboardAvoidingView, View, Text, FlatList, ActivityIndicator } from 'react-native';
-
-import { withNavigation } from 'react-navigation';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
+import { withNavigation , navigation } from 'react-navigation';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import HeaderButtons from 'react-navigation-header-buttons';
 
 import styles from './friends.style';
 import theme from '../../styles/theme.style';
 
+// TODO Add Backend Data Implementation to Populate Data
+
 class Friends extends React.Component {
 
+    // Declare Edit Friend Icon
+    static navigationOptions = ({ navigation }) => {
+
+      // TODO Move Edit Friend to Header
+      return {
+        title: 'Friends',
+        headerRight: (
+          <HeaderButtons IconComponent={MaterialIcons} iconSize={23} color={theme.COLOR_PRIMARY_DARK}>
+          <HeaderButtons.Item 
+            title='EditFriends'
+            iconName='person-add'
+            onPress={() => { /*TODO Add Edit Friend Functionality*/ }}/>
+          </HeaderButtons>
+        ),
+      };
+    };
+
   render() {
+
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Friends!</Text>
--- src/pages/journals/journals.index.js
+++ src/pages/journals/journals.index.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import { KeyboardAvoidingView, View, Text, FlatList, ActivityIndicator } from 'react-native';
-
-import { withNavigation } from 'react-navigation';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
+import { withNavigation , navigation } from 'react-navigation';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import HeaderButtons from 'react-navigation-header-buttons';
+import { Icon } from 'native-base';
 
 import styles from './journals.style';
 import theme from '../../styles/theme.style';
 
+// TODO Add Backend Data Implementation to Populate Data
+
 class Journals extends React.Component {
+  
+  // Declare Settings Icon
+  static navigationOptions = ({ navigation }) => {
+
+    // TODO Move Settings Gear to Header
+    return {
+      title: 'Journals',
+      headerRight: (
+        <HeaderButtons IconComponent={MaterialIcons} iconSize={23} color={theme.COLOR_PRIMARY_DARK}>
+        <HeaderButtons.Item 
+          title='EditJournals'
+          iconName='book'
+          onPress={() => { /*TODO Add Edit Journal Functionality*/ }}/>
+        </HeaderButtons>
+      ),
+    };
+
+  };
 
   render() {
     return (
